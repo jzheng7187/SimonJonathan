8d44ea0b2dbd9b65cd0f14f0b8624784aac20eb3
afsanalove <afsanalove@169.254.115.63>
2017-01-10 23:05:27 -0500

    just started

---
+++ SimonJonathan/src/PartnerCodeInHerePlease/Move.java
@@ -0,0 +1,14 @@
+package PartnerCodeInHerePlease;
+
+import SimonJonathan.ButtonInterfaceJonathanZ;
+import SimonJonathan.MoveInterfaceJonathanZ;
+
+public class Move implements MoveInterfaceJonathanZ {
+
+	@Override
+	public ButtonInterfaceJonathanZ getButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

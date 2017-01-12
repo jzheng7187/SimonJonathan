f5e184e18179422b372062d414b660b6a2950b37
Student7 <Student7@10.8.33.151>
2017-01-12 11:58:03 -0500

    meh

---
+++ SimonJonathan/src/PartnerCodeInHerePlease/Move.java
@@ -0,0 +1,18 @@
+package PartnerCodeInHerePlease;
+
+import SimonJonathan.ButtonInterfaceJonathanZ;
+import SimonJonathan.MoveInterfaceJonathanZ;
+
+public class Move implements MoveInterfaceJonathanZ {
+
+	public Move() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ButtonInterfaceJonathanZ getButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ SimonJonathan/src/PartnerCodeInHerePlease/Progress.java
@@ -0,0 +1,67 @@
+package PartnerCodeInHerePlease;
+
+import java.awt.image.BufferedImage;
+
+import SimonJonathan.ProgressInterfaceJonathanZ;
+
+public class Progress implements ProgressInterfaceJonathanZ {
+
+	public Progress() {
+		
+	}
+
+	@Override
+	public int getHeight() {
+		
+		return 0;
+	}
+
+	@Override
+	public BufferedImage getImage() {
+		
+		return null;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isAnimated() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setRound(int b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSequenceSize(int b) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
